1488e929dc375cbdbf37514f9cb27b4c1bf1b211
riansco14 <rian.rabelo@ufrpe.br>
2020-04-20 02:49:24 -0300

    Criação de header menu e instrutores

--- Project02-Design-Backend/routes.js
+++ Project02-Design-Backend/routes.js
@@ -2,11 +2,11 @@ const express=require('express');
 const routes=express.Router();
 
 routes.get('/', function (req, res) {
-    return res.redirect('/intrutores');
+    return res.redirect('/instrutores');
 });
 
-routes.get('/intrutores', function(req,res) {
-    return res.render("intrutores/index");
+routes.get('/instrutores', function(req,res) {
+    return res.render("instrutores/index");
 });
 
 routes.get('/membros', function(req,res) {
