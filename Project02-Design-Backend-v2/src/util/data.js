7df5e3a66dd19a09dc138e550d329572c48c2962
riansco14 <rian.rabelo@ufrpe.br>
2020-05-05 06:20:26 -0300

    Versão 2 - Sistema academia

---
+++ Project02-Design-Backend-v2/src/util/data.js
@@ -0,0 +1,31 @@
+module.exports = {
+    idade(timestamp) {
+        let dataAtual = new Date();
+        let dataNascimento = new Date(timestamp);
+
+        let idade = dataAtual.getFullYear() - dataNascimento.getFullYear();
+
+        const mesVariation = dataAtual.getMonth() - dataNascimento.getMonth();
+        const diaVariation = dataAtual.getDate() - dataNascimento.getDate();
+
+        if (mesVariation <= 0 && diaVariation < 0) {
+            idade -= 1;
+        }
+
+        return idade;
+    },
+    date(timestamp) {
+        const formattedDate = new Date(timestamp);
+        ano = formattedDate.getUTCFullYear();
+        mes = String("0" + (formattedDate.getUTCMonth() + 1)).slice(-2);
+        dia = String("0" + formattedDate.getUTCDate()).slice(-2);
+
+        return {
+            dia,
+            mes,
+            ano,
+            iso: `${ano}-${mes}-${dia}`,
+            aniversario: `${mes}/${ano}`
+        };
+    }
+}
