c4c9d629148e5335b8cb33eb1c7aef1be7216f1d
riansco14 <rian.rabelo@ufrpe.br>
2020-05-14 06:33:03 -0300

    CSS do Form de Produto

--- Project03-Store/src/routes.js
+++ Project03-Store/src/routes.js
@@ -1,9 +1,14 @@
 const express=require('express');
 const routes=express.Router();
+const ProdutoController=require('./app/controllers/ProdutoController');
 
 
 routes.get('/', function (req, res) {
     return res.render('layout');
 });
+
+routes.get('/produtos/create', ProdutoController.create);
+
+routes.get('/ads/create')
 
 module.exports=routes;
